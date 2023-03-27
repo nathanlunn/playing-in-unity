439f62dd847660011b9a91c00d7ca8ef215611f0
Nathan Lunn <nathandouglaslunn@gmail.com>
2023-03-27 05:23:56 -0400

    send a message to the console when the game starts

--- playing-in-udemy/Assets/PrintToConsole.cs
+++ playing-in-udemy/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Come on man! You can do it!");
     }
 
     // Update is called once per frame
